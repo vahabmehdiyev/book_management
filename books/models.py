64e07c2d5d2999446d44ecb228a2932af1b195c0
Vahab <108904622+vahabmehdiyev@users.noreply.github.com>
2023-07-10 03:33:22 +0400

    book

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    
---
+++ books/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.book_list, name='book-list')
+    
+    
+]
